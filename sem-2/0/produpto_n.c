d3ae769e2091ed86e558f9487eab7bd60e5c228d
Krishna Yadav <73414562+kr812345@users.noreply.github.com>
2025-02-24 01:16:12 +0530

    Delete sem-2 directory

--- sem-2/0/produpto_n.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int array[4] = {1,2,3,4};
-    return 0;
-}
--- sem-2/0/sumupto_n.java
+++
@@ -1,5 +0,0 @@
-public class sumupto_n{
-    public static void main(String[] args){
-        System.out.println("@Krishna!");
-    }
-}
